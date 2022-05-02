1991b97d8331b8fbe06f4bcd1f1571e9f37866b5
ZAliaks <zui@tut.by>
2022-05-02 12:13:10 +0300

    feat: решил еще пару задачек

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let result = names.reduce((acc, el) => {
+    let name = el,
+      count = 0
+    while (acc.includes(name)) {
+      count++
+      name = `${el}(${count})`
+    }
+    acc.push(name)
+    return acc
+  }, [])
+  return result
 }
 
 module.exports = {
-  renameFiles
-};
+  renameFiles,
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let resArr = []
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+      i++
+    } else {
+      if (arr[i] === '--discard-prev' &&  resArr.length !== 0 & resArr.includes(arr[i - 1])
+      ){
+        resArr.pop()       
+      } else {
+        if (arr[i] === '--double-next' && i !== arr.length - 1) {
+          resArr.push(arr[i + 1])
+        } else {
+          if (arr[i] === '--double-prev' && resArr !== 0 && resArr.includes(arr[i - 1])
+          ) {
+            resArr.push(resArr[resArr.length - 1])           
+          } else {
+            if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+            resArr.push(arr[i])}
+          }
+        }
+      }
+    }
+  }
+  return resArr
 }
 
 module.exports = {
-  transform
-};
+  transform,
+}
